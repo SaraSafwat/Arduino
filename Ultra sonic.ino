a33c94a73bed189a438853de72e3bed8b1fcefab
sara <36546098+SaraSafwat@users.noreply.github.com>
2018-02-22 11:37:51 -0800

    Add files via upload

---
+++ Ultra sonic.ino
@@ -0,0 +1,55 @@
+int buzzpin=8;
+int greenledpin=11;
+int redledpin1= 9;
+int redledpin2= 5;
+int echopin=6;
+int trigpin= 7;
+
+void setup()
+{
+
+Serial.begin (9600);
+pinMode (buzzpin,OUTPUT); 
+pinMode (greenledpin,OUTPUT );
+pinMode (redledpin1 ,OUTPUT );
+pinMode (redledpin2 ,OUTPUT );
+
+pinMode (echopin,INPUT);
+pinMode (trigpin, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite (trigpin ,LOW );
+  delayMicroseconds (3);
+  digitalWrite (trigpin , HIGH );
+  delayMicroseconds (3);
+  digitalWrite (trigpin , LOW ); 
+
+pinMode (echopin,INPUT);
+long di = pulseIn (echopin , HIGH );
+long distance= (di/2)/29 ;
+
+  
+ if (distance < 0.5 )
+ {
+  digitalWrite (redledpin1, HIGH );
+   digitalWrite (redledpin2, HIGH );
+
+  digitalWrite (buzzpin, HIGH );  
+  digitalWrite (greenledpin , LOW );
+  }
+else 
+{
+   digitalWrite (greenledpin, HIGH );
+  digitalWrite (buzzpin, LOW );
+  digitalWrite (redledpin1, LOW);
+    digitalWrite (redledpin2, LOW);
+
+  
+  
+  }
+
+
+  
+}
