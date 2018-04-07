bb5ca939d438a6a558dfe1325a34dd3e1b933048
sara <36546098+SaraSafwat@users.noreply.github.com>
2018-04-07 10:59:38 -0700

    bluetooth module code testting

---
+++ blutooth.ino
@@ -0,0 +1,24 @@
+#include <SoftwareSerial.h>
+SoftwareSerial myserial (8,9);
+
+
+void setup() {
+myserial.begin(9600);
+Serial.begin(9600);
+}
+
+
+
+void loop() {
+  if(myserial.available())
+{
+  Serial.write(myserial.read());
+}
+    if (Serial.available() )
+    {
+      myserial.write(Serial.read() );
+    }
+  }
+  // put your main code here, to run repeatedly:zd
+
+
